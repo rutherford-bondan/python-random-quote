cb833dc8f4f54878595ec4dfbdc598ac9ab0e663
Austin Bond <austinbond98@gmail.com>
2020-11-22 17:32:42 -0700

    Extra changes plus buggy quotes

--- get-quote.py
+++ get-quote.py
@@ -3,13 +3,18 @@ import random
 def primary():
   #print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
+  f = open("quotes.txt", "a")
+  f.write("Oops, propagating bug")
+  f.close()
+
+  f = open("quotes.txt", "r")
   quotes = f.readlines()
   f.close()
 
   last = len(quotes) - 1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  rnd2 = random.randint(0, last)
+  print(quotes[rnd], quotes[rnd2], sep = '', end = '')
 
 if __name__== "__main__":
   primary()
